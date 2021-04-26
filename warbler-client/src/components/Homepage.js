04cc59218cf43f2b602abbd0a4e9023346a46713
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2021-04-26 15:53:07 +0000

    css file

--- warbler-client/src/components/Homepage.js
+++ warbler-client/src/components/Homepage.js
@@ -1,11 +1,14 @@
+// stateless functional component
 import React from "react";
 import { Link } from "react-router-dom";
+// style home-hero in index.css
 
 const Homepage = () => {
   return (
     <div className="home-hero">
       <h1>What's Happening?</h1>
       <h4>New to Warbler?</h4>
+      {/* we don't have any of these routes built yet */}
       <Link to="/signup" className="btn btn-primary">
         Sign up here
       </Link>
--- warbler-client/src/containers/App.js
+++ warbler-client/src/containers/App.js
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import { configureStore } from "../store";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Navbar";
+// import Main
 import Main from "./Main";
 
 const store = configureStore();
@@ -12,6 +13,8 @@ const App = () => (
     <Router>
       <div className="onboarding">
         <Navbar />
+        {/* Main component*/}
+        {/* components/ folder make a new file Homepage.js*/}
         <Main />
       </div>
     </Router>
